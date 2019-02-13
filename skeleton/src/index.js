518880e51dbccbf00c6c26a3dd2af33b036162f9
jason-t-fu <jasonfu1992@gmail.com>
2019-02-12 16:51:54 -0800

    Remove renamed files

--- skeleton/src/index.js
+++
@@ -1,9 +0,0 @@
-const View = require("./ttt-view.js");// require appropriate file
-const Game = require("../../solution/game.js");// require appropriate file
-
-  $(() => {
-    const $el = $('.ttt');
-    const game = new Game();
-    const view = new View(game, $el);
-    
-  });
